c70c2d02816c65923c083c055e46ec6266a0eb86
Vitalii Kaplenko <eps_kvv@yahoo.com>
2022-02-26 20:57:24 +0200

    Prototyp: Edit puppeteer scripts

--- Prototyping/get-pdf-stealth-before.js
+++ Prototyping/get-pdf-stealth-before.js
@@ -5,11 +5,5 @@ puppeteer.use(pluginStealth);
 (async() => {
     const browser = await puppeteer.launch({headless:false, userDataDir: "./user_data"});
     const page = await browser.newPage();
-    //await page.setViewport({width: 800, height: 600});
-    //await page.goto('https://stackoverflow.com/questions/tagged/c%2b%2b11');
     await page.goto('https://www.learncpp.com/');
-    //await page.goto('file:///media/dii/92ce574f-1df5-455f-993e-c55a62bf4a30/home/dii/Documents/dii/jobs/gl/BaseCamp/21q3/solutions/html2pdf/Prototyping/learncpp.html');
-    //await page.goto('https://datadome.co/');
-    // await page.pdf({path: 'some.pdf'});
-    // browser.close();
 })();
